bfe1769233a0abf8f8a8a18c0676ed5e91b74936
sufyana35 <sufyana35@gmail.com>
2021-09-05 17:49:36 +0100

    Store blog posts voa html files

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts');
 });
+
+Route::get('posts/{post}', function ($slug) {
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    if(! file_exists($path)) {
+        return redirect('/');
+        //abort(404);
+    }
+
+    $post = file_get_contents($path);
+
+    return view('post', [
+        'post' => $post
+    ]);
+});
