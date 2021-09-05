fb6b3c0b75dd3b2c427953cdee7e206e098031f5
sufyana35 <sufyana35@gmail.com>
2021-09-05 17:13:21 +0100

    make a route and link it

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1>My First Post</h1>
+
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Aliquam sem et tortor consequat id porta. Iaculis urna id volutpat lacus laoreet non.
+        </p>
+    </article>
+</body>
